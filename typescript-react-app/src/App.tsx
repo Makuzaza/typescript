708af9beec0c07e5cfcee93ddff7bc8585da4e51
Makuzaza <maria.kuznetsova@edu.bc.fi>
2024-02-06 18:02:58 +0200

    new

--- typescript-react-app/src/App.tsx
+++ typescript-react-app/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.modules';
 
 // function App() {
 //   return (
@@ -10,13 +11,73 @@ import NewTodo from './components/NewTodo';
 // }
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish Typescript course' }]
+  // const todos = [{ id: 't1', text: 'Finish Typescript course' }]
+  
+  const [todoState, setTodoState] = useState<string>('add');
+  const [todo, setTodo] = useState<Todo>({ id: '', text: '' });
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  return <div className='App'>
-    {/* A component that adds a todo */}
-    <NewTodo />
-    <TodoList items = {todos}/>
-    Typescript - React</div>
+  const todoAddHandler = (text: string) => {
+    if (todoState === 'edit') {
+      setTodos((prevTodos) => {
+        return prevTodos.map((prevTodo) => {
+          if (prevTodo.id === todo.id) {
+            return todo;
+          } else {
+            return prevTodo;
+          }
+        });
+      });
+    } else {
+      setTodos((prevTodos) => [
+        ...prevTodos,
+        { id: Math.random().toString(), text: text },
+      ]);
+    }
+    setTodo((prevTodo) => {
+      return { ...prevTodo, id: '', text: '' };
+    });
+    setTodoState(() => {
+      return '';
+    });
+  }
+
+  const todoChangeHandler = (text: string) => {
+    setTodo((prevTodo) => {
+      return { ...prevTodo, text: text };
+    });
+  };
+  const todoEditHandler = (id: string) => {
+    setTodo((prevTodo) => {
+      const todo = todos.filter((todo) => todo.id === id);
+      const text = todo.length ? todo[0].text : '';
+      return { ...prevTodo, id: id, text: text };
+    });
+    setTodoState(() => {
+      return 'edit';
+    });
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
+  }
+
+  return (
+    <div className="App">
+    <NewTodo
+      text={todo.text}
+      state={todoState}
+      onChangeTodo={todoChangeHandler}
+      onAddTodo={todoAddHandler}
+    />
+    <TodoList
+      items={todos}
+      onEditTodo={todoEditHandler}
+      onDeleteTodo={todoDeleteHandler}
+    />
+  </div>)
 }
 
 export default App;
--- typescript-react-app/src/components/NewTodo.tsx
+++ typescript-react-app/src/components/NewTodo.tsx
@@ -1,22 +1,44 @@
 import React, { useRef } from 'react';
+import './newTodo.css';
 
-const NewTodo: React.FC = () => {
+type NewTodoProps = {
+    state: string;
+    text: string;
+    onAddTodo: (todoText: string) => void; 
+    onChangeTodo: (todoText: string) => void; 
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
 
     const textInputRef = useRef<HTMLInputElement>(null);
     
     const todoSubmitHandler = (event: React.FormEvent) => {
         event.preventDefault();
-        const enteredText = textInputRef.current!.value;
-        console.log(enteredText);
+        const enteredText = textInputRef.current!.value.trim();
+        if (enteredText) props.onAddTodo(enteredText);
     }
+
+    const todoChangeHandler = (event: React.ChangeEvent) => {
+        event.preventDefault();
+        props.onChangeTodo((event.target as HTMLInputElement).value);
+      };
 
   return (
   <form onSubmit={todoSubmitHandler}>
     <div>
         <label htmlFor='todo-text'>Todo Text</label>
-        <input type='text' id='todo-text' ref={textInputRef} />
+        <input
+          type="text"
+          id="todo-text"
+          ref={textInputRef}
+          value={props.text}
+          onChange={todoChangeHandler}
+        />
     </div>
-    <button type='submit'>Add Todo</button>
+    <div>
+        {props.state !== 'edit' && <button type="submit">Add Todo</button>}
+        {props.state === 'edit' && <button type="submit">Update Todo</button>}
+      </div>
   </form>
   );
 }
